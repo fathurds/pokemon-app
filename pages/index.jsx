91e43a1684b7c19fa6d3532814bb2974d151fb33
Fathurrachman D. Saputra <namasayafathur@gmail.com>
2022-05-15 22:55:33 +0700

    Adding detail

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,35 @@
 import Layout from "../components/Layout"
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home({ pokemon }) {
-
   return (
     <Layout title="Home">
       <h1 className="text-5xl font-bold text-center py-5 text-slate-700">Fath Pokedex</h1>
-      {console.log(pokemon)}
       <div className="grid gap-2 sm:grid-cols-2 sm:gap-2 lg:grid-cols-4 xl:grid-cols-5">
         {pokemon.map((el, i) => (
-          <div key={i}>
+          <div>
             {/* Mobile */}
             <Link href={`/pokemon/${i + 1}`}>
-              <div className="border bg-slate-200 flex items-center justify-around lg:hidden">
-                <img src={el.image} className='w-28 lg:justify-self-center' />
+              <div className="border bg-slate-200 flex items-center justify-around lg:hidden cursor-pointer ">
+                <img src={el.image} className='w-28 lg:justify-self-center' alt={el.name} />
                 <h4 className='capitalize font-bold text-2xl text-slate-700'>{el.name}</h4>
               </div>
             </Link>
 
             {/* Dekstop */}
             <Link href={`/pokemon/${i + 1}`}>
-              <div className="hidden border bg-slate-200 lg:block">
-                <img src={el.image} className='w-28 mx-auto' />
+              <div className="hidden border bg-slate-200 lg:block cursor-pointer hover:scale-150 hover:border-slate-300 transition-all duration-300">
+                <img src={el.image} className='w-28 mx-auto' alt={el.name} />
                 <h4 className='capitalize font-bold text-2xl text-slate-700 text-center'>{el.name}</h4>
               </div>
             </Link>
           </div>
         ))}
+        <div>
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
       </div>
     </Layout>
   )
--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -1,39 +1,82 @@
 import Layout from "../../components/Layout";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function PokemonDetail() {
+export default function PokemonDetail({ pokemonDetail }) {
+    const router = useRouter();
+    const { id } = router.query;
+
+    const pokemonName = pokemonDetail.name.charAt(0).toUpperCase() + pokemonDetail.name.slice(1);
+
     return (
-        <Layout title="Detail">
-            Pokemon Detail
+        <Layout title={pokemonName}>
+            <h1 className="capitalize text-4xl text-center">{pokemonDetail.name}</h1>
+            <div className="flex justify-around">
+                <img src={pokemonDetail.image} alt={pokemonDetail.name} className="w-60" />
+                <div className="my-auto px-10">
+                    <h4>HP: </h4>
+                    <progress id="health" value={pokemonDetail.stats[0].base_stat} max="150"></progress><br />
+
+                    <h4>Atack: </h4>
+                    <progress id="health" value={pokemonDetail.stats[1].base_stat} max="150"></progress><br />
+
+                    <h4>Defence: </h4>
+                    <progress id="health" value={pokemonDetail.stats[2].base_stat} max="150"></progress><br />
+
+                    <h4>Special Attack: </h4>
+                    <progress id="health" value={pokemonDetail.stats[3].base_stat} max="150"></progress><br />
+
+                    <h4>Special Defence: </h4>
+                    <progress id="health" value={pokemonDetail.stats[4].base_stat} max="150"></progress><br />
+
+                    <h4>Speed: </h4>
+                    <progress id="health" value={pokemonDetail.stats[5].base_stat} max="150"></progress><br />
+
+
+                </div>
+            </div>
+            <div className="bg-sky-400 rounded-xl flex justify-center gap-10 w-10/12 mx-auto py-3">
+                <div>
+                    <h4 className="text-slate-100 font-semibold">Height</h4>
+                    <h4 className="font-semibold text-slate-700 text-center">{pokemonDetail.height} cm</h4>
+                </div>
+                <div>
+                    <h4 className="text-slate-100 font-semibold">Weight</h4>
+                    <h4 className="font-semibold text-slate-700 text-center">{pokemonDetail.weight} kg</h4>
+                </div>
+                <div>
+                    <h4 className="text-slate-100 font-semibold text-center">Type</h4>
+                    <h4 className="capitalize font-semibold text-slate-700 text-center">
+                        {pokemonDetail.types.map((el, i) => {
+                            return " " + pokemonDetail.types[i].type.name;
+                        })}
+                    </h4>
+
+                </div>
+            </div>
+            <Link href={`/pokemon/${pokemonDetail.id - 1}`}>
+                <button className="border bg-slate-400 py-2 px-3 mt-3 rounded-lg" disabled={id == 1 ? true : false} >Previous</button>
+            </Link>
+            <Link href={`/pokemon/${pokemonDetail.id + 1}`}>
+                <button className="border bg-slate-400 py-2 px-3 mt-3 rounded-lg float-right" disabled={id == 898 ? true : false}>Next</button>
+            </Link>
+
+
         </Layout>
     )
 }
 
-// export async function getStaticPaths() {
-//     const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-//     const data = await response.json();
+export async function getServerSideProps({ query }) {
+    const id = query.id;
 
-//     const paths = data.results.map((el) => {
-//         return {
-//             params: {
-//                 id: String(el.id),
-//             },
-//         };
-//     });
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokemonDetail = await res.json();
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+    pokemonDetail.image = image;
 
-//     return {
-//         paths,
-//         fallback: false,
-//     };
-// }
-
-// export async function getStaticProps(context) {
-//     const { id } = context.params;
-//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-//     const pokemonDetail = await response.json();
-
-//     return {
-//         props: {
-//             pokemonDetail,
-//         },
-//     };
-// }
+    return {
+        props: {
+            pokemonDetail,
+        }
+    }
+}
